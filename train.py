7b8f61a16b34121e2c6505126d0ffbdc671cd4fc
Rodrigo Rivera <rgrivera@uc.cl>
2019-01-08 03:23:44 +0100

    fixed dataset

--- train.py
+++ train.py
@@ -1,32 +1,29 @@
 from __future__ import print_function
 
 
-
 import tensorflow as tf
 import numpy as np
-import pickle
-import vgg
+import tensorflow.contrib.slim.nets
 from dataset import Dataset
 
 
-
 NUM_EPOCHS = 100
-BATCH_SIZE = 512
+BATCH_SIZE = 256
 NUM_CLASSES = 100
-NUM_ITERATIONS = NUM_TRAIN_SAMPLES // BATCH_SIZE
+IMG_SIZE = 224
 LEARNING_RATE = 0.00001
 
-    
 train = Dataset('cifar-100-python/train')
 
-rgb = tf.placeholder(tf.float32, shape=[None, 32, 32, 3])
+rgb = tf.placeholder(tf.float32, shape=[None, IMG_SIZE, IMG_SIZE, 3])
 y = tf.placeholder(tf.int64, shape=[None])
 
 with tf.name_scope('model'):
-    logits, __ = vgg_16(rgb, num_classes=NUM_CLASSES)
+    logits, __ = tf.contrib.slim.nets.vgg.vgg_16(rgb, num_classes=NUM_CLASSES)
 
 with tf.name_scope('loss'):
-    cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=logits)
+    cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y,
+                                                                   logits=logits)
     loss = tf.reduce_mean(cross_entropy, name='loss')
 
 with tf.name_scope('optimizer'):
@@ -37,12 +34,12 @@ with tf.name_scope('optimizer'):
 
 saver = tf.train.Saver()
 
+num_iterations = train.num_samples // BATCH_SIZE
+
 with tf.Session() as sess:
     saver.restore(sess, 'cifar-100-python/vgg_16.ckpt')
-    for i in range(NUM_ITERATIONS):
+    for i in range(num_iterations):
         rgb_batch, y_batch = train.get_batch(BATCH_SIZE)
-        sess.run(opt_op, feed_dict={rgb: rgb_batch, y: y_batch})
+        rgb_batch = tf.image.resize_images(rgb_batch, [IMG_SIZE, IMG_SIZE])
+        sess.run(full_opt_op, feed_dict={rgb: rgb_batch, y: y_batch})
         saver.saver(sess, 'saved/model.ckpt')
-
-
-
